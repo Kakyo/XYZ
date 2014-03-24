cc81576ef897c4960ef9764d42636d8c48689d32
Luís Alberto Dias Pinheiro <luis.diaspinheiro@gmail.com>
2014-03-23 21:39:26 -0300

    add Parceiro service

---
+++ XYZ/XYZ.Services.Parceiro/ParceiroService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace XYZ.Services.Parceiro
+{
+    public class ParceiroService : IParceiroService
+    {
+        public bool Ping()
+        {
+            return true;
+        }
+    }
+}
